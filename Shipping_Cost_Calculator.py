5be27a51a20a08c62d9802a15201f41ec6966d19
aniqatasnia <tas.aniqa@gmail.com>
2026-04-23 15:40:00 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 79726be92ad97197e1a93ffd54a3adcd03140324.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by aniqatasnia
- # Here is another update by <your GitHub username>
+
 
